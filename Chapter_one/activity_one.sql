cb257cd9cf565e90402530e7f14079b0d1af1317
HeartyTjan <salamitijani02@gmail.com>
2025-01-14 19:23:44 +0100

    Update

---
+++ Chapter_one/activity_one.sql
@@ -0,0 +1,10 @@
+SELECT * FROM heartycustomers.customersinfo;
+
+INSERT INTO customersinfo
+VALUES(1, 'Salami', 'Tijani', 'Software Enginer', 'Nigeria'),
+	  (2, 'Ahmed', 'Tobi', 'Software Enginer', 'Nigeria'),
+      (3, 'Ahmed', 'Tobi', 'Software Enginer', 'Nigeria');
+      
+INSERT INTO customersinfo (id, FirstName,LastName,JobTitle,Country)
+VALUES(5,'Fumilayo','Owolarafe', 'Software Engineer','Canada');
+
